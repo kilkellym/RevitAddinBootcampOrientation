8091e6e1cd5034dc3f4846899a12db3e76f696f1
kilkellym <michael@archsmarter.com>
2025-03-31 16:18:06 -0400

    Add project files.

---
+++ RevitAddinBootcampOrientation/cmdSkills01.cs
@@ -0,0 +1,28 @@
+﻿namespace RevitAddinBootcampOrientation
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdSkills01 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string myVar = "this is my first variable";
+            TaskDialog.Show("Test", "This is a new message from me.");
+
+            TaskDialog.Show("Test", "This is another dialog to test");
+
+
+            // Your Module 01 Skills code goes here
+            // Delete the TaskDialog below and add your code
+            //TaskDialog.Show("Module 01 Skills", "Got Here!");
+
+
+            return Result.Succeeded;
+        }
+    }
+
+}
